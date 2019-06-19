05f5ca179d2c3cd962fefe926eb56d9a5497ac07
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2019-06-19 12:26:56 +1000

    added latest screenfull and anuglar 8

--- src/app/image-viewer/fullscreen.directive.ts
+++ src/app/image-viewer/fullscreen.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, HostListener, OnChanges, Input, ElementRef } from '@angular/core';
-import * as screenfull from 'screenfull';
+import * as screenfull from "screenfull";
+import {Screenfull} from "screenfull";
 
 @Directive({
     selector: '[ngxToggleFullscreen]'
@@ -12,10 +13,10 @@ export class ToggleFullscreenDirective implements OnChanges {
     constructor(private el: ElementRef) { }
 
     ngOnChanges() {
-        if (this.isFullscreen && screenfull.enabled) {
-            screenfull.request(this.el.nativeElement);
-        } else if (screenfull.enabled) {
-            screenfull.exit();
+        if (this.isFullscreen && (<Screenfull>screenfull).enabled) {
+            (<Screenfull>screenfull).request(this.el.nativeElement);
+        } else if ((<Screenfull>screenfull).enabled) {
+            (<Screenfull>screenfull).exit();
         }
     }
 
